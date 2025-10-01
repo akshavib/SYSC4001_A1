6cfbb4da8ed172a8fa110e5f1def41650748ad90
akshavibaskaran <akshavi.bt@gmail.com>
2025-10-01 15:49:03 -0400

    SYSCALL method

--- interrupts.cpp
+++ interrupts.cpp
@@ -23,23 +23,36 @@ int main(int argc, char** argv) {
 
     /******************ADD YOUR VARIABLES HERE*************************/
     int curr_time = 0; // current time in the simulation
+    int cntxt_time = 10; // default 10ms for saving context
 
     /******************************************************************/
 
     //parse each line of the input trace file
     while(std::getline(input_file, trace)) {
-        auto [activity, duration_intr] = parse_trace(trace);
+        auto [activity, duration_intr] = parse_trace(trace); // duration_intr is the duration of the activity or the interrupt number
 
         /******************ADD YOUR SIMULATION CODE HERE*************************/
         if(activity == "CPU"){
             // CPU activity - string format: <time>, <duration>, CPU activity
-            execution += std::to_string(curr_time) + ", " + std::to_string(duration_intr) + ", CPU activity\n";
+            execution += std::to_string(curr_time) + ", " + std::to_string(duration_intr) + ", CPU burst\n";
         
         } else if (activity == "SYSCALL"){
-            execution += std::to_string(curr_time) + ", " + std::to_string(duration_intr) + ", CPU activity\n";
-            
-        } else if(activity == "END_IO") {
+            // boilerplate for syscall interrupt
+            auto [logging, upd_time] = intr_boilerplate(curr_time, duration_intr, cntxt_time, vectors);
+            curr_time = upd_time;
+            execution += logging;
 
+            // ISR body (wait time for that device)
+            int wait = delays[duration_intr];
+            curr_time += wait;
+            execution += std::to_string(curr_time) + ", " + std::to_string(wait) + ", ISR for device " + std::to_string(duration_intr) + "\n";
+
+            // return from interrupt boilerplate
+            curr_time++;
+            execution += std::to_string(curr_time) + ", " + std::to_string(1) + ", return from interrupt\n";
+
+
+        } else if(activity == "END_IO") {
         }
         
 
